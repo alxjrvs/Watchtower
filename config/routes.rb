d3b46f52d9c177fbb17e12da0eec6078cb783d13
alxjrvs <alxjrvs@gmail.com>
2012-08-22 01:11:46 -0400

    Bootstrap default layout included and configured.

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Watchtower::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'user#index'
+  root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
