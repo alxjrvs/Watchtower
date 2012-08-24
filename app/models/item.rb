18fed2330e18a16cd6719c44b1f1ad4648f556fa
alxjrvs <alxjrvs@gmail.com>
2012-08-24 14:39:34 -0400

    added assign_date method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ActiveRecord::Base
+
+  after_save :assign_date
+
   attr_accessible :date_assigned, :name
   belongs_to :user
 
@@ -10,5 +13,13 @@ class Item < ActiveRecord::Base
       a << i.type.to_s.downcase.pluralize
     end
     a.uniq
+  end
+
+  private
+
+  def assign_date
+    if self.user
+      self.update_attributes(:date_assigned => Time.now) unless self.date_assigned
+    end
   end
 end
